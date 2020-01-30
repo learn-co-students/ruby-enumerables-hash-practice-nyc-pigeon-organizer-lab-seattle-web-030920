0334b1c26b0f9e477d3f895eb15ae573eaa08b93
Christine Bumpous <bumpouce@gmail.com>
2020-01-30 00:42:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  reorganized_pigeon_data = {}
+
+  data.collect do |category|
+    key = category.shift
+    category.collect do |type|
+      type.collect do |names|
+        descriptor = names.shift.to_s
+        list_of_names = names.shift
+        list_of_names.collect do |pigeon|
+
+          if reorganized_pigeon_data.has_key?(pigeon)
+            if reorganized_pigeon_data[pigeon].has_key?(key)
+              reorganized_pigeon_data[pigeon][key] << descriptor
+            else
+            reorganized_pigeon_data[pigeon][key] = [descriptor]
+            end
+         else
+            reorganized_pigeon_data[pigeon] = {}
+            reorganized_pigeon_data[pigeon][key] = [descriptor]
+          end
+          
+        end
+      end
+    end
+  end
+  reorganized_pigeon_data
 end
