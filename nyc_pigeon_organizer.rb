41e4b2ae33fc461b922166056cf690acb2de4106
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-15 00:50:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigion_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names| 
+      all_names.each do |name|
+        if pigion_list[name] == nil 
+          pigion_list[name] = {}
+        end 
+        if pigion_list[name][color_gender_lives] == nil
+          pigion_list[name][color_gender_lives] = []
+        end 
+        pigion_list[name][color_gender_lives].push(stats.to_s)
+      end 
+    end
+  end
+  pigion_list
 end
